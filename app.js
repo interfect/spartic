6ed950845cb012dea5b9ac06635e5bda7e1036ba
Adam Novak <interfect@gmail.com>
2022-12-14 19:04:13 -0500

    Start using Hyperswarm

---
+++ app.js
@@ -0,0 +1,24 @@
+import {SparticPeer} from './index.js'
+import crypto from 'hypercore-crypto'
+
+const peer1 = new SparticPeer()
+const peer2 = new SparticPeer()
+
+// Topics are 32 bytes
+const topic = crypto.randomBytes(32)
+
+console.log('Connecting server')
+const discovery = peer1.join(topic, { server: true, client: false })
+// Wait for the topic to be fully announced on the DHT
+console.log('Waiting for server to announce...')
+await discovery.flushed() 
+
+console.log('Connecting client')
+peer2.join(topic, { server: false, client: true })
+// Wait for the swarm to connect to pending peers.
+console.log('Waiting for client to connect...')
+await peer2.flush()
+
+console.log('Connection should be up')
+
+// After this point, both client and server should have connections
---
+++ index.js
@@ -0,0 +1,46 @@
+import Hyperswarm from 'hyperswarm'
+import crypto from 'hypercore-crypto'
+
+/// How long are crypto key seeds?
+export const SEED_SIZE = 32
+
+/// Turn a buffer into a hex string
+function key_to_string(key) {
+  return key.toString("hex")
+}
+
+/// Turn a buffer into a short hex string
+function key_to_name(key) {
+  return key.slice(0, 4).toString("hex")
+}
+
+export class SparticPeer extends Hyperswarm {
+  /// Make a new SparticPeer.
+  /// Takes options defined for a Hyperswarm, such as a 32-byte buffer "seed"
+  /// for making the private key.
+  constructor(options) {
+    super(options)
+    this.log('My public key is ', this.keyPair.publicKey)
+    
+    this.on('connection', (conn, info) => {
+      this.log('Connected to: ', key_to_name(info.publicKey))
+      conn.on('data', (data) => {
+        this.log('<-', '(' + key_to_name(info.publicKey) + ')', data.toString())
+      })
+      conn.write('Hello peer!')
+    })
+    
+  }
+  
+  /// Get a short name for this peer defined by its key
+  getName() {
+    return key_to_name(this.keyPair.publicKey)
+  }
+  
+  /// Log a message to the console, noting it came from this peer
+  log(...args) {
+    console.log('(' + this.getName() + ')', ...args)
+  }
+
+}
+
