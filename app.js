42752b45522fa03a0f7b2799a69a8c958900a10c
Adam Novak <interfect@github.com>
2022-12-23 12:55:29 -0600

    Change to use Protomux

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ const peer2 = new SparticPeer()
 // Topics are 32 bytes
 const topic = crypto.randomBytes(32)
 
-
-console.log('Connecting server')
-const discovery = peer1.join(topic, { server: true, client: false })
-// Wait for the topic to be fully announced on the DHT
-console.log('Waiting for server to announce...')
-await discovery.flushed() 
+console.log('Starting server listener')
+await peer1.listen()
 
 console.log('Connecting client')
-peer2.join(topic, { server: false, client: true })
+peer2.joinPeer(peer1.keyPair.publicKey)
+
 // Wait for the swarm to connect to pending peers.
 console.log('Waiting for client to connect...')
 await peer2.flush()
--- src/spartic_peer.js
+++ src/spartic_peer.js
@@ -1,5 +1,8 @@
 import Hyperswarm from 'hyperswarm'
 
+import Protomux from 'protomux'
+import cenc from 'compact-encoding'
+
 /**
  * Main peer class.
  *
@@ -28,17 +31,58 @@ export default class SparticPeer extends Hyperswarm {
   constructor(options) {
     super(options)
     this.log('My public key is ', this.keyPair.publicKey)
-    
+
+    // We need a table to map from connected peer pubkey to fancy struct, and
+    // so we can drop them when the peer goes away.
+    // TODO: Do we really have 1 connection per pubkey? 
+    this._messengers = {}
+
     this.on('connection', (conn, info) => {
       this.log('Connected to: ', this.constructor.keyToName(info.publicKey))
-      conn.on('data', (data) => {
-        this.log('<-', '(' + this.constructor.keyToName(info.publicKey) + ')', data.toString())
+      // Connections are already message-oriented, length-prefixed streams of entire buffers. 
+      // We need a real protocol over this though, since among other things we might need to be in multiple simultaneous groups with the same peer.
+      // So define a protocol with Protomux
+      let mux = new Protomux(conn)
+      let channel = mux.createChannel({
+        userData: this,
+        protocol: 'spartic',
+        onopen: () => {
+          // The channel is open.
+          // Could have a handshake argument
+          this.log('Opened Spartic channel with (' + this.constructor.keyToName(info.publicKey) + ')')
+        },
+        onclose: () => {
+          // The channel is closed.
+          this.log('Closed Spartic channel with (' + this.constructor.keyToName(info.publicKey) + ')')
+        }
       })
-      conn.write('Hello peer!')
+      let textMessage = channel.addMessage({
+        encoding: cenc.utf8,
+        onmessage: async (message) => {
+          // A message has arrived
+          this.log('<-', '(' + this.constructor.keyToName(info.publicKey) + ')', message)
+        }
+      })
+
+      // Save the fancy channel thingy
+      this._messengers[info.publicKey] = {
+        mux: mux,
+        channel: channel,
+        textMessage: textMessage
+      }
+      
+      conn.on('close', () => {
+        // When the connection closes, remove the fancy channel thingy
+        delete this._messengers[info.publicKey]
+      })
+      
+      // TODO: Protomux API docs say we should open the message, but really we open the channel.
+      channel.open()
+      textMessage.send('Hello peer!')
     })
     
   }
-  
+
   /// Get a short name for this peer defined by its key
   getName() {
     return this.constructor.keyToName(this.keyPair.publicKey)
